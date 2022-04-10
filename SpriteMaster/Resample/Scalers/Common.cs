03ce6f90991f267bbfc8ebe257c652ebe20ff6cc
Ameisen <14104310+ameisen@users.noreply.github.com>
2022-04-10 15:03:06 -0500

    Adding a ColorDistance method.

---
+++ SpriteMaster/Resample/Scalers/Common.cs
@@ -0,0 +1,34 @@
+﻿using SpriteMaster.Colors;
+using SpriteMaster.Types;
+using static SpriteMaster.Colors.ColorHelpers;
+
+namespace SpriteMaster.Resample.Scalers;
+
+static class Common {
+	internal static uint ColorDistance(
+		bool useRedmean,
+		bool gammaCorrected,
+		bool hasAlpha,
+		in Color16 pix1,
+		in Color16 pix2,
+		in YccConfig yccConfig
+	) {
+		if (useRedmean) {
+			return ColorHelpers.RedmeanDifference(
+				pix1,
+				pix2,
+				linear: !gammaCorrected,
+				alpha: hasAlpha
+			);
+		}
+		else {
+			return ColorHelpers.YccDifference(
+				pix1,
+				pix2,
+				config: yccConfig,
+				linear: !gammaCorrected,
+				alpha: hasAlpha
+			);
+		}
+	}
+}
