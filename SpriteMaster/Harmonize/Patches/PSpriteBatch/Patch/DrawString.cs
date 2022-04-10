3dc3c609f5e9735a1157f04bfe7bf034355818d9
Ameisen <14104310+ameisen@users.noreply.github.com>
2022-04-10 15:07:43 -0500

    Adding resampling to DrawString.

---
+++ SpriteMaster/Harmonize/Patches/PSpriteBatch/Patch/DrawString.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Text;
+
+namespace SpriteMaster.Harmonize.Patches.PSpriteBatch.Patch;
+
+internal class DrawString {
+	// public unsafe void DrawString (SpriteFont spriteFont, string text, Vector2 position, Color color)
+	// public unsafe void DrawString (SpriteFont spriteFont, string text, Vector2 position, Color color, float rotation, Vector2 origin, Vector2 scale, SpriteEffects effects, float layerDepth)
+	// public unsafe void DrawString (SpriteFont spriteFont, StringBuilder text, Vector2 position, Color color)
+	// public unsafe void DrawString (SpriteFont spriteFont, StringBuilder text, Vector2 position, Color color, float rotation, Vector2 origin, Vector2 scale, SpriteEffects effects, float layerDepth)
+
+	[Harmonize("DrawString", priority: Harmonize.PriorityLevel.First)]
+	public static bool OnDrawString(SpriteBatch __instance, SpriteFont spriteFont, string text, XNA.Vector2 position, XNA.Color color) {
+		__instance.DrawString(
+			spriteFont: spriteFont,
+			text: text,
+			position: position,
+			color: color,
+			rotation: 0.0f,
+			origin: XNA.Vector2.Zero,
+			scale: XNA.Vector2.One,
+			effects: SpriteEffects.None,
+			layerDepth: 0.0f
+		);
+		return false;
+	}
+
+	[Harmonize("DrawString", priority: Harmonize.PriorityLevel.First)]
+	public static bool OnDrawString(SpriteBatch __instance, SpriteFont spriteFont, StringBuilder text, XNA.Vector2 position, XNA.Color color) {
+		__instance.DrawString(
+			spriteFont: spriteFont,
+			text: text,
+			position: position,
+			color: color,
+			rotation: 0.0f,
+			origin: XNA.Vector2.Zero,
+			scale: XNA.Vector2.One,
+			effects: SpriteEffects.None,
+			layerDepth: 0.0f
+		);
+		return false;
+	}
+
+	[Harmonize("DrawString", priority: Harmonize.PriorityLevel.Last)]
+	public static bool OnDrawString(
+		SpriteBatch __instance,
+		SpriteFont spriteFont,
+		string text,
+		XNA.Vector2 position,
+		XNA.Color color,
+		float rotation,
+		XNA.Vector2 origin,
+		XNA.Vector2 scale,
+		SpriteEffects effects,
+		float layerDepth
+	) {
+		return Core.OnDrawStringImpl.DrawString(
+			__instance,
+			spriteFont,
+			text,
+			position,
+			color,
+			rotation,
+			origin,
+			scale,
+			effects,
+			layerDepth
+		);
+	}
+
+	[Harmonize("DrawString", priority: Harmonize.PriorityLevel.Last)]
+	public static bool OnDrawString(
+		SpriteBatch __instance,
+		SpriteFont spriteFont,
+		StringBuilder text,
+		XNA.Vector2 position,
+		XNA.Color color,
+		float rotation,
+		XNA.Vector2 origin,
+		XNA.Vector2 scale,
+		SpriteEffects effects,
+		float layerDepth
+	) {
+		return Core.OnDrawStringImpl.DrawString(
+			__instance,
+			spriteFont,
+			text,
+			position,
+			color,
+			rotation,
+			origin,
+			scale,
+			effects,
+			layerDepth
+		);
+	}
+}
