ec0d223990fc3b0f34850d80fda1cd0d3c6c69e6
Ameisen <14104310+ameisen@users.noreply.github.com>
2022-05-06 21:25:01 -0500

    Fixing shift error in Numeric/Float.

--- SpriteMaster/Numeric/Float.cs
+++ SpriteMaster/Numeric/Float.cs
@@ -30,10 +30,10 @@ internal static class Float {
 	internal static class Double {
 		internal const uint Bits = 64;
 		internal const uint ExponentBits = 11;
-		internal const ulong ExponentMask = (1U << (int)ExponentBits) - 1;
+		internal const ulong ExponentMask = (1UL << (int)ExponentBits) - 1;
 		internal const ulong ExponentMaskShifted = ExponentMask << (int)SignificandBits;
 		internal const uint SignificandBits = 52;
-		internal const ulong SignificandMask = (1U << (int)SignificandBits) - 1;
+		internal const ulong SignificandMask = (1UL << (int)SignificandBits) - 1;
 		internal const uint MantissaBits = SignificandBits;
 
 		internal static double Make(uint exponent, ulong significand) => (((ulong)exponent << (int)SignificandBits) | significand).ReinterpretAs<double>();
