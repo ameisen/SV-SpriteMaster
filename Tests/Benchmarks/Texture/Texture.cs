439fdf3098e286a79aec9d2fade4c155179a8016
Ameisen <14104310+ameisen@users.noreply.github.com>
2022-05-15 18:39:09 -0500

    Work on texture benchmark

---
+++ Tests/Benchmarks/Texture/Texture.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Texture;
+
+public class Texture {
+	//[STAThread]
+	public static int Main(string[] args) {
+		_ = BenchmarkRunner.Run<Tests>();
+		return 0;
+	}
+}
