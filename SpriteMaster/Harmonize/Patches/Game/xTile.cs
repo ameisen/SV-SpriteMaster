60acd2fd384152ab18bf19c2f383c44a143fb2ae
Ameisen <14104310+ameisen@users.noreply.github.com>
2023-01-29 16:19:25 -0600

    Adding debugging code to try to find out why AT causes a Span2D Exception. Clamping Fixed-Point divides to fix red highlight issue. Adding concurrency patch to xTile to try to fix exception... again.

---
+++ SpriteMaster/Harmonize/Patches/Game/xTile.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading;
+
+namespace SpriteMaster.Harmonize.Patches.Game;
+
+internal static class xTile {
+
+	[Harmonize(
+		"xTile.Tiles.TileIndexPropertyAccessor",
+		"get_Item",
+		Harmonize.Fixation.Prefix,
+		Harmonize.PriorityLevel.First,
+		critical: false
+	)]
+	public static void DeprecationManagerWarnPre(
+		object __instance,
+		object ___m_cache,
+		int tileIndex
+	) {
+		Monitor.Enter(___m_cache);
+	}
+
+	[Harmonize(
+		"xTile.Tiles.TileIndexPropertyAccessor",
+		"get_Item",
+		Harmonize.Fixation.Finalizer,
+		Harmonize.PriorityLevel.Last,
+		critical: false
+	)]
+	public static void DeprecationManagerWarnPost(
+		object __instance,
+		object ___m_cache,
+		int tileIndex
+	) {
+		Monitor.Exit(___m_cache);
+	}
+
+	[Harmonize(
+		"xTile.Tiles.TileIndexPropertyCollection",
+		"IndexKey",
+		Harmonize.Fixation.Prefix,
+		Harmonize.PriorityLevel.First,
+		critical: false
+	)]
+	public static void DeprecationManagerWarnPre(
+		object __instance,
+		object ___m_indexKeys,
+		string key
+	) {
+		Monitor.Enter(___m_indexKeys);
+	}
+
+	[Harmonize(
+		"xTile.Tiles.TileIndexPropertyCollection",
+		"IndexKey",
+		Harmonize.Fixation.Finalizer,
+		Harmonize.PriorityLevel.Last,
+		critical: false
+	)]
+	public static void DeprecationManagerWarnPost(
+		object __instance,
+		object ___m_indexKeys,
+		string key
+	) {
+		Monitor.Exit(___m_indexKeys);
+	}
+}
